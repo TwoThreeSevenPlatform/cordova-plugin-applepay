80b61c2409419d33c047c756cb61b9d23651d954
Devin Smith <github@arzynik.com>
2015-08-28 17:43:40 -0700

    removed more debug stuff

--- src/ios/CDVApplePay.m
+++ src/ios/CDVApplePay.m
@@ -58,13 +58,8 @@
         CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString: @"user has apple pay"];
         [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
     } else {
-#if DEBUG
-        CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString: @"in debug mode, simulating apple pay"];
-        [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
-#else
         CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString: @"user does not have apple pay"];
         [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
-#endif
     }
 }
 
